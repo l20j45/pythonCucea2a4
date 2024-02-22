22a7a77db9aa20ef66de8a8c4668cb62a54958a8
daniel <l20j45_@hotmai.com>
2024-02-22 15:33:33 -0600

    media clase 22 while

---
+++ bucles while for/ManzanasW2.py
@@ -0,0 +1,20 @@
+descuento = 0
+cantidadDeManzanas = 1
+
+while cantidadDeManzanas != 0:
+    cantidadDeManzanas= float(input("Ingresa cuantas manzanas vas a vender"))
+    if cantidadDeManzanas == 0:
+        break
+    precioDeLasManzanas= float(input("Ingresa el precio de las manzanas"))
+    if cantidadDeManzanas == 18 :
+        descuento = (cantidadDeManzanas * precioDeLasManzanas) *.15
+        print(f"el descuento es de {descuento}")
+    elif cantidadDeManzanas >= 10 :
+        descuento = (cantidadDeManzanas * precioDeLasManzanas) *.10
+        print(f"el descuento es de {descuento}")
+    else:
+        print("gracias por comprar")
+    totalAPagar = (cantidadDeManzanas * precioDeLasManzanas) - descuento
+
+    print(f"Total a pagar {totalAPagar} " )
+print("Adio")
--- bucles while for/tablaWhile.py
+++ bucles while for/tablaWhile.py
@@ -1,7 +1,8 @@
 multiplicador = 2
 multiplicando = 0
 while multiplicando <= 100:
-    print(f"{multiplicador} x {multiplicando} = {multiplicando*multiplicador} ")
-    #multiplicando = multiplicando + 1
-    multiplicando += 2
-
+    if multiplicando != 50:
+        print(f"{multiplicador} x {multiplicando} = {multiplicando*multiplicador} ")
+        #multiplicando = multiplicando + 1
+    multiplicando += 1    
+        
